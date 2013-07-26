cc0e2cb0ffbc1464e1da4096dc56eab39fbb1ee4
Christine Coulter <christinetcoulter@gmail.com>
2013-07-25 20:45:50 -0400

    Add prompts to get endTrain and endStation.

--- mta.js
+++ mta.js
@@ -61,7 +61,10 @@ var msg1 = "Which train would you like to get on?\n" + displayLines();
 var startTrain = prompt(msg1);
 var msg2 = "At which station would you like to get on?\n" + displayStations();
 var startStation = prompt(msg2);
-
+var msg3 = "Which train would you like to get off?\n" + displayLines();
+var endTrain = prompt(msg3);
+var msg4 = "At which station would you like to get off?\n" + displayStations();
+var endStation = prompt(msg4);
 
 
 
